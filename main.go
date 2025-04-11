f08af9d396ba53d797e3f763b5676e187c8d5380
Artem Parshin <parart1@yandex.ru>
2025-04-11 22:09:51 +0300

    refactor(main): remove ide comments

--- main.go
+++ main.go
@@ -10,9 +10,6 @@ import (
 	"tochecken/tgbot"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
 func main() {
 	ru, err := checkers.NewRubUsd()
 	if err != nil {
@@ -103,9 +100,6 @@ func main() {
 
 	}(a, b)
 
-	fmt.Println("Запуск бота v1.1.2...")
+	fmt.Println("Bot start...")
 	b.Bot.Start()
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
